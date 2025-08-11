85e42783330012e12dce1b00186776061e51b2b4
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-08-11 20:22:50 +0700

    Day 02

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-import http from "http";
+import express from "express";
+import { createResponse } from "./src/common/configs/response.config.js";
 
-const foods = [
-	{
-		id: 1,
-		title: "Suon Xao chua ngot",
-		price: 100,
-	},
-	{
-		id: 2,
-		title: "nuoc loc",
-		price: 4,
-	},
+const users = [
+	{ id: 1, name: "Hoang", age: 33 },
+	{ id: 2, name: "MindX", age: 10 },
+	{ id: 3, name: "Khoa", age: 18 },
 ];
 
-const callback = (request, response) => {
-	console.log(typeof response);
-	console.log({ response });
-	response.write("hello");
-	response.write("hello");
-	response.write("hello");
-	response.write("hello");
-	response.end("Hello mindX");
-	// response.end("Hello mindX");
-};
+const app = express();
 
-// * Định tuyến = phân chia các công việc thành các tuyến đường riêng để xử lý tuỳ theo yêu cầu từ client.
+// * middleware -> học kỹ sau.
+app.use(express.json());
 
-// GET, POST, PUT, PATCH, DELETE
-const app = http.createServer(callback);
+// * res.json() hay res.send() đều kết thúc request
+// * res.json() được tối ưu hơn cho định dạng JSON.
+// * res.send() tự động biến đổi dữ liệu khi response
+
+app.get("/users/:id", (req, res) => {
+	console.log(req.params);
+	const user = users.find((item) => item.id == req.params.id);
+	return createResponse(res, 200, "Lấy user theo id thành công!", user);
+});
+
+// * /users?maxAge=20
+app.get("/users", (req, res) => {
+	console.log(req.query);
+});
+
+app.post("/auth/login", (req, res) => {
+	console.log(req.body);
+});
 
 app.listen(8080, () => {
 	console.log("Server is running!");
 });
+
+/**
+ * * Có 3 cách để gửi dữ liệu từ client về server
+ * * req.params (/:id)
+ * * req.query (?query1=value1&query2=value2)
+ * * req.body (cần sử dụng middleware express.json())
+ *
+ */
